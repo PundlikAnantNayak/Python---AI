461ca1d62780c7af66affd9835c3a94f6b48272d
Pundlik Anant Nayak <92310214+PundlikAnantNayak@users.noreply.github.com>
2022-04-28 18:52:29 +0530

    Created SetOperations.py
    This program demonstrates operations on set.

---
+++ SetOperations.py
@@ -0,0 +1,6 @@
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
+print("Union of E and N is",E | N) #Union
+print("Intersection of E and N is",E & N) #Intersection
+print("Difference of E and N is",E - N) #Difference
+print("Symmetric difference of E and N is",E ^ N) #Symmetric Difference
